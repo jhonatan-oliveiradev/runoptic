6d79f7565e96e501b28a711b16e03dff271e442e
Jhonatan Oliveira <jhonatan.oliveira931002@gmail.com>
2026-09-19 17:10:00 -0300

    chore: migrate RunOptic runtime identity

--- windows/codenotch/src/config.rs
+++ windows/codenotch/src/config.rs
@@ -169,7 +169,7 @@ impl Default for Config {
 pub fn config_path() -> PathBuf {
     dirs::config_dir()
         .unwrap_or_else(|| PathBuf::from("."))
-        .join("codenotch")
+        .join("runoptic")
         .join("config.json")
 }
 
